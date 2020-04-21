36ea208880f91bd9abbf6a349a66995c6b089fd6
berkaykarlik <brkykrlk@gmail.com>
2020-04-21 16:02:11 +0300

    Url dao and repo added

---
+++ src/main/java/com/ariba/spring/url/shortener/Repository/UrlRepository.java
@@ -0,0 +1,11 @@
+package com.ariba.spring.url.shortener.Repository;
+
+
+import com.ariba.spring.url.shortener.model.Url;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface UrlRepository extends MongoRepository<Url,String> {
+    Url findByUrl(String url);
+
+}
